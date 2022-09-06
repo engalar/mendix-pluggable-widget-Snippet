89de28d959ff0805decca69dec5259d7637a0b1d
engalar <engalar@gmail.com>
2022-09-06 14:32:31 +0800

    feat: mount event

--- src/Snippet.tsx
+++ src/Snippet.tsx
@@ -1,21 +1,33 @@
-// import { useMemo } from "react";
 import { SnippetContainerProps } from "../typings/SnippetProps";
-// import { ValueStatus } from "mendix";
 import classNames from "classnames";
 
 import "./ui/index.scss";
+import { useEffect, useRef, useState } from "react";
+import { useMount } from "ahooks";
+
+import { set } from 'dojo/html';
 
 export default function (props: SnippetContainerProps) {
-    console.log(props);
-    // const value = useMemo(() => {
-    //     if (props.attribute && props.attribute.status === ValueStatus.Available) {
-    //         return props.attribute.value;
-    //     }
-    // }, [props.attribute]);
+    const ref = useRef<HTMLDivElement>(null);
+
+    const [hasTriggerMount, setHasTriggerMount] = useState(false);
+
+    useMount(() => {
+        set(ref.current!, props.htmlContent);
+    });
+    useEffect(() => {
+        if (!hasTriggerMount && props.mountAction?.canExecute && !props.mountAction.isExecuting) {
+            setHasTriggerMount(true);
+            props.mountAction.execute();
+        }
+        return () => {
+
+        }
+    }, [props.mountAction])
+
 
     return (
-        <div style={props.style} className={classNames("mendixcn-graph", props.class)}>
-            hello {props.sampleText} and your value is
+        <div ref={ref} style={props.style} className={classNames("mendixcn-snippet", props.class)}>
         </div>
     );
 }
--- typings/SnippetProps.d.ts
+++ typings/SnippetProps.d.ts
@@ -1,22 +1,25 @@
 /**
- * This file was generated from Graph.xml
+ * This file was generated from Snippet.xml
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
 import { CSSProperties } from "react";
+import { ActionValue } from "mendix";
 
-export interface GraphContainerProps {
+export interface SnippetContainerProps {
     name: string;
     class: string;
     style?: CSSProperties;
     tabIndex?: number;
-    sampleText: string;
+    htmlContent: string;
+    mountAction?: ActionValue;
 }
 
-export interface GraphPreviewProps {
+export interface SnippetPreviewProps {
     className: string;
     style: string;
     styleObject?: CSSProperties;
     readOnly: boolean;
-    sampleText: string;
+    htmlContent: string;
+    mountAction: {} | null;
 }
